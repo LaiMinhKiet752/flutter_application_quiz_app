761a1fda10f6671d366b41961fc2514a8b32667a
Kiet <kietminh070502@gmail.com>
2022-12-15 22:31:46 +0700

    purchase history

--- lib/screen/history_screen.dart
+++ lib/screen/history_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/screen/home.dart';
+import 'package:quiz_app/services/localdb.dart';
 
 // ignore: must_be_immutable
 class HistoryScreen extends StatefulWidget {
@@ -18,6 +19,28 @@ class HistoryScreen extends StatefulWidget {
 }
 
 class _HistoryScreenState extends State<HistoryScreen> {
+  String name = '';
+  String proUrl = '';
+  getUserInfo() async {
+    await LocalDB.getName().then((value) {
+      setState(() {
+        name = value.toString();
+      });
+    });
+
+    await LocalDB.getUrl().then((value) {
+      setState(() {
+        proUrl = value.toString();
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,18 +55,183 @@ class _HistoryScreenState extends State<HistoryScreen> {
         backgroundColor: Color.fromARGB(255, 251, 100, 90),
         title: Text(
           "Most recent purchase history",
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
         ),
         centerTitle: true,
+        elevation: 0.0,
       ),
       body: Container(
-        padding: EdgeInsets.only(left: 5.0, right: 5.0),
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.only(top: 100.0),
         child: ListView.builder(
           itemCount: 1,
-          itemBuilder: (context, index) => Card(
-            child: ListTile(
-              title: Text("Code orders: ${widget.code}"),
-              subtitle: Text(widget.buy_at.toDate().toString()),
-              trailing: Text("Conis: ${widget.money}"),
+          itemBuilder: (context, index) => Container(
+            padding: EdgeInsets.all(10.0),
+            height: 400.0,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 120.0,
+                        height: 120.0,
+                        child: Image.asset("assets/img/logo_quiz.png"),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              "INVOICE",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Invoice #: ${widget.code}",
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Container(
+                              width: 120.0,
+                              child: Text(
+                                "Place: 65 Huynh Thuc Khang, Ben Nghe Ward, District 1, Ho Chi Minh City",
+                                softWrap: true,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  indent: 5.0,
+                  endIndent: 5.0,
+                  thickness: 2.0,
+                  color: Colors.grey,
+                ),
+                Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Provided by:",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text("KDV QUIZ COMPANY"),
+                            Text("65 Huynh Thuc Khang"),
+                            Text("Ben Nghe Ward"),
+                            Text("District 1"),
+                            Text("Ho Chi Minh City"),
+                          ],
+                        ),
+                      ),
+                      Expanded(child: Container()),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Provided to:",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              name,
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                            CircleAvatar(
+                              radius: 29.0,
+                              backgroundColor: Colors.black,
+                              child: CircleAvatar(
+                                radius: 28.0,
+                                backgroundImage: proUrl.isNotEmpty
+                                    ? NetworkImage(proUrl)
+                                    : null,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Text("DESCRIPTION"),
+                      ),
+                      Container(
+                        child: Text("Created_at"),
+                      ),
+                      Container(
+                        child: Text("Total Coins"),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 100.0,
+                        child: Text("Payment of game items"),
+                      ),
+                      Container(
+                        width: 100.0,
+                        child: Text("${widget.buy_at.toDate().toString()}"),
+                      ),
+                      Container(
+                        child: Text("${widget.money} Coins"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
